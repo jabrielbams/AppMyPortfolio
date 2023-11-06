fdce9041d5fdd30daf5c84c286bca1271b360ac5
jabrielbams <jabrielbams@gmail.com>
2023-11-06 15:33:36 +0700

    first version

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React from "react";
+import { Alert, TouchableOpacity, Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { ButtonMain } from "./components/atomics";
+
+import Login from "./screens/login";
+import Register from "./screens/register";
+import Home from "./screens/home";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+ return (
+  <NavigationContainer>
+   <Stack.Navigator>
+    <Stack.Group>
+     <Stack.Screen
+      name='Login'
+      component={Login}
+      options={{
+       title: "Login Screen",
+      }}
+     />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+     <Stack.Screen
+      name='Register'
+      component={Register}
+      options={{
+       title: "Register Screen",
+       headerLeft: null,
+      }}
+     />
+
+     <Stack.Screen
+      name='Home'
+      component={Home}
+      options={({ navigation }) => ({
+       title: "Home",
+       headerLeft: null,
+       headerRight: () => {
+        return (
+         <TouchableOpacity
+          style={{ paddingHorizontal: 20 }}
+          onPress={() => {
+           Alert.alert("Logout Berhasil");
+           navigation.dispatch(StackActions.replace("Login"));
+          }}>
+          <Text>Logout</Text>
+         </TouchableOpacity>
+        );
+       },
+      })}
+     />
+    </Stack.Group>
+   </Stack.Navigator>
+  </NavigationContainer>
+ );
+}
